7d4cd20554aae8a8778fc193233955c2a1d7ca3d
baskota-adarsha <privateadarsha@gmail.com>
2025-11-13 12:35:57 +0545

    Update connectDB.ts

--- backend/connectDB/connectDB.ts
+++ backend/connectDB/connectDB.ts
@@ -5,7 +5,10 @@ export const connectDB = async (): Promise<void> => {
     if (process.env.MONGO_URI === undefined) {
       return;
     }
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI,{
+  serverSelectionTimeoutMS: 30000, // Increase timeout
+  socketTimeoutMS: 45000,
+});
     console.log(`MongoDB connected successfully`);
   } catch (error) {
     console.log(error);
